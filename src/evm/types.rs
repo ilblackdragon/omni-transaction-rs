f4e4dc69a8cf32be2212eeba04d4feb3c7183274
edsonalcala <edson_alcala@msn.com>
2024-08-27 00:36:39 +0100

    serialize signature

--- src/evm/types.rs
+++ src/evm/types.rs
@@ -1,7 +1,11 @@
+use near_sdk::serde::{Deserialize, Serialize};
+
 pub type Address = [u8; 20];
 
 pub type AccessList = Vec<(Address, Vec<[u8; 32]>)>;
 
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Signature {
     pub v: u64,
     pub r: Vec<u8>,
