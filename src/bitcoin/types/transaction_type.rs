fa5238f7c50d1c12aa53e3ee335d24f3e8107c86
edsonalcala <edson_alcala@msn.com>
2024-09-24 02:14:00 +0100

    fix: cleaning code

--- src/bitcoin/types/transaction_type.rs
+++ src/bitcoin/types/transaction_type.rs
@@ -1,6 +1,10 @@
 pub enum TransactionType {
+    /// Pay to public key hash
     P2PKH,
+    /// Pay to script hash
     P2SH,
+    /// Pay to witness public key hash
     P2WPKH,
+    /// Pay to witness script hash
     P2WSH,
 }
--- tests/bitcoin_integration_test.rs
+++ tests/bitcoin_integration_test.rs
@@ -144,6 +144,124 @@ async fn test_send_p2pkh_using_rust_bitcoin_and_omni_library() -> Result<()> {
     Ok(())
 }
 
+#[tokio::test]
+async fn test_send_p2wpkh_using_rust_bitcoin_and_omni_library() -> Result<()> {
+    let bitcoind = bitcoind::BitcoinD::from_downloaded().unwrap();
+    let client: &bitcoind::Client = &bitcoind.client;
+
+    // Setup testing environment
+    let mut btc_test_context = BTCTestContext::new(client).unwrap();
+
+    // Setup Bob and Alice addresses
+    let bob = btc_test_context.setup_account().unwrap();
+
+    let alice = btc_test_context.setup_account().unwrap();
+
+    // Generate 101 blocks to the address
+    client.generate_to_address(101, &bob.address)?;
+
+    // List UTXOs for Bob
+    let unspent_utxos_bob = btc_test_context.get_utxo_for_address(&bob.address).unwrap();
+
+    // Get the first UTXO
+    let first_unspent = unspent_utxos_bob
+        .into_iter()
+        .next()
+        .expect("There should be at least one unspent output");
+
+    let txid_str = first_unspent["txid"].as_str().unwrap();
+    let bitcoin_txid: bitcoin::Txid = txid_str.parse()?;
+    let omni_hash = OmniHash::from_hex(txid_str)?;
+    let omni_txid = OmniTxid(omni_hash);
+
+    assert_eq!(bitcoin_txid.to_string(), omni_txid.to_string());
+
+    let vout = first_unspent["vout"].as_u64().unwrap();
+
+    // Create inputs using Omni library
+    let txin: OmniTxIn = OmniTxIn {
+        previous_output: OmniOutPoint::new(omni_txid, vout as u32),
+        script_sig: OmniScriptBuf::default(), // Initially empty, will be filled later with the signature
+        sequence: OmniSequence::MAX,
+        witness: OmniWitness::default(),
+    };
+
+    let txout = OmniTxOut {
+        value: OMNI_SPEND_AMOUNT,
+        script_pubkey: OmniScriptBuf(alice.script_pubkey.as_bytes().to_vec()),
+    };
+
+    let utxo_amount =
+        OmniAmount::from_sat((first_unspent["amount"].as_f64().unwrap() * 100_000_000.0) as u64);
+
+    let change_amount: OmniAmount = utxo_amount - OMNI_SPEND_AMOUNT - OmniAmount::from_sat(1000); // 1000 satoshis for fee
+
+    let change_txout = OmniTxOut {
+        value: change_amount,
+        script_pubkey: OmniScriptBuf(bob.script_pubkey.as_bytes().to_vec()),
+    };
+
+    let mut omni_tx: BitcoinTransaction = TransactionBuilder::new::<BITCOIN>()
+        .version(OmniVersion::One)
+        .lock_time(OmniLockTime::from_height(1).unwrap())
+        .inputs(vec![txin])
+        .outputs(vec![txout, change_txout])
+        .build();
+
+    // Add the script_sig to the transaction
+    omni_tx.input[0].script_sig = OmniScriptBuf(bob.script_pubkey.as_bytes().to_vec());
+
+    // Extend the transaction with the sighash type
+    let sighash_type = OmniSighashType::All;
+    let encoded_data = omni_tx.build_for_signing(sighash_type);
+
+    // Calculate the sighash
+    let sighash_omni = sha256d::Hash::hash(&encoded_data);
+    let msg_omni = Message::from_digest_slice(sighash_omni.as_byte_array()).unwrap();
+
+    // Sign the sighash and broadcast the transaction using the Omni library
+    let secp = Secp256k1::new();
+    let signature_omni = secp.sign_ecdsa(&msg_omni, &bob.private_key);
+
+    // Verify signature
+    let is_valid = secp
+        .verify_ecdsa(&msg_omni, &signature_omni, &bob.public_key)
+        .is_ok();
+
+    assert!(is_valid, "The signature should be valid");
+
+    // Encode the signature
+    let signature = bitcoin::ecdsa::Signature {
+        signature: signature_omni,
+        sighash_type: EcdsaSighashType::All,
+    };
+
+    // Create the script_sig
+    let script_sig_new = Builder::new()
+        .push_slice(signature.serialize())
+        .push_key(&bob.bitcoin_public_key)
+        .into_script();
+
+    // Assign script_sig to txin
+    let omni_script_sig = OmniScriptBuf(script_sig_new.as_bytes().to_vec());
+    let encoded_omni_tx = omni_tx.build_with_script_sig(0, omni_script_sig, TransactionType::P2PKH);
+
+    // Convert the transaction to a hexadecimal string
+    let hex_omni_tx = hex::encode(encoded_omni_tx);
+
+    let raw_tx_result: serde_json::Value = client
+        .call("sendrawtransaction", &[json!(hex_omni_tx)])
+        .unwrap();
+
+    println!("raw_tx_result: {:?}", raw_tx_result);
+
+    client.generate_to_address(101, &bob.address)?;
+
+    assert_utxos_for_address(client, alice.address, 1);
+
+    Ok(())
+}
+
 fn assert_utxos_for_address(client: &bitcoind::Client, address: Address, number_of_utxos: usize) {
     let min_conf = 1;
     let max_conf = 9999999;
