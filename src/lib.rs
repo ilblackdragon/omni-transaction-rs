e159acd99e785dc4372d7324898dc12d1069d267
edsonalcala <edson_alcala@msn.com>
2024-09-26 07:00:20 +0100

    fix: added features and test new bin

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+#[cfg(feature = "bitcoin")]
 pub mod bitcoin;
-pub mod constants;
+#[cfg(feature = "evm")]
 pub mod evm;
+#[cfg(feature = "near")]
 pub mod near;
+
+pub mod constants;
 pub mod transaction_builder;
 pub mod types;
