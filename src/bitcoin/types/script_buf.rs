7754817e4bd4f6cd9e9eb17796ee99d6037d040e
edsonalcala <edson_alcala@msn.com>
2024-09-26 07:40:21 +0100

    fix: added from_hex to scriptbuf

--- src/bitcoin/types/script_buf.rs
+++ src/bitcoin/types/script_buf.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::io::{BufRead, Write};
 
 use borsh::{BorshDeserialize, BorshSerialize};
@@ -9,6 +10,37 @@ use crate::bitcoin::encoding::{encode::Encodable, Decodable};
     Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize, BorshSerialize, BorshDeserialize,
 )]
 pub struct ScriptBuf(pub Vec<u8>);
+
+impl ScriptBuf {
+    /// Creates a [`ScriptBuf`] from a hex string.
+    pub fn from_hex(s: &str) -> Result<Self, String> {
+        let v = Vec::from_hex(s)?;
+        Ok(ScriptBuf::from_bytes(v))
+    }
+
+    /// Converts byte vector into script.
+    ///
+    /// This method doesn't (re)allocate.
+    pub fn from_bytes(bytes: Vec<u8>) -> Self {
+        ScriptBuf(bytes)
+    }
+}
+
+pub trait FromHex: Sized {
+    /// Error type returned while parsing hex string.
+    type Error: Sized + fmt::Debug + fmt::Display;
+
+    /// Produces an object from a hex string.
+    fn from_hex(s: &str) -> Result<Self, Self::Error>;
+}
+
+impl FromHex for Vec<u8> {
+    type Error = String;
+
+    fn from_hex(s: &str) -> Result<Self, Self::Error> {
+        hex::decode(s).map_err(|e| e.to_string())
+    }
+}
 
 impl Encodable for ScriptBuf {
     fn encode<W: Write + ?Sized>(&self, w: &mut W) -> Result<usize, std::io::Error> {
